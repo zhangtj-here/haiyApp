0ef55ba27acb5bc08f0e5d2a1354eeccd0cd3788
zhangtj <1771988873@qq.com>
2019-04-13 21:40:13 +0800

    init file

---
+++ src/components/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="container" :style="{backgroundImage: 'url(' + img + ')' }">
+    <img class="logo" src="@/assets/logocnodc.png">
+    <el-row id="formRow">
+      <el-col :span="24">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="用户名">
+            <el-input
+              placeholder="请输入用户名"
+              v-model="form.name"
+              clearable>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input
+              placeholder="请输入密码"
+              type="password"
+              v-model="form.password"
+              clearable>
+            </el-input>
+          </el-form-item>
+          <el-button type="primary" plain>登录</el-button>
+          <el-button type="warning" plain>登录</el-button>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import Img from '@/assets/7.jpg'
+
+export default {
+  data () {
+    return {
+      img: Img,
+      form: {
+        name: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div#container{
+    height: 100%;
+    padding-top: 30px;
+    background-position: center center;
+    background-repeat:  no-repeat;
+    background-size: auto 100%;
+    box-sizing: border-box;
+  }
+  .logo{
+    width: 150px;
+  }
+  #formRow{
+    padding: 0 10px;
+    margin-top: 20px;
+  }
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
