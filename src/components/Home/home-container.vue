9737327135a8541bd32d22dd8a9feddb6f9464b0
zhangtj <1771988873@qq.com>
2019-04-16 09:28:29 +0800

    首页顶部样式添加

--- src/components/Home/home-container.vue
+++ src/components/Home/home-container.vue
@@ -4,7 +4,28 @@
       <router-link to="/" slot="left">
       </router-link>
     </mt-header>
-    <div ></div>
+    <div id="topLink">
+      <div>
+        <img src="@/assets/logo.png" alt="">
+        111
+      </div>
+      <div>
+        <img src="@/assets/logo.png" alt="">
+        222
+      </div>
+      <div>
+        <img src="@/assets/logo.png" alt="">
+        222
+      </div>
+      <div>
+        <img src="@/assets/logo.png" alt="">
+        222
+      </div>
+      <div>
+        <img src="@/assets/logo.png" alt="">
+        222
+      </div>
+    </div>
 
   </div>
 </template>
@@ -19,5 +40,19 @@ export default {
 </script>
 
 <style scoped>
-
+  #topLink{
+    display: flex;
+    flex-flow: row  nowrap;
+  }
+  #topLink div{
+    display: flex;
+    flex: auto;
+    flex-direction: column;
+    height: 100px;
+    align-items: center;
+    justify-content: center;
+  }
+  #topLink div img{
+    height: 60px;
+  }
 </style>
